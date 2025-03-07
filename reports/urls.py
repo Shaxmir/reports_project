bfcf88e60d22e52062934e923b5700c26028ade0
SoDDoS <shaxrussian@gmail.com>
2025-03-07 13:00:00 +0300

    Откат

--- reports/urls.py
+++ reports/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SaleViewSet, ExpenseViewSet, CashRegisterViewSet, daily_report, add_expense, update_cash, update_expense
+from .views import SaleViewSet, ExpenseViewSet, CashRegisterViewSet, daily_report, add_expense, update_cash
 
 router = DefaultRouter()
 router.register(r'sales', SaleViewSet)
@@ -11,7 +11,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/report/', daily_report, name="daily_report"),
     path('api/expenses/', add_expense, name="add_expense"),
-    path("api/expenses/<int:expense_id>/", update_expense, name="update_expense"),
     path('api/cash/', update_cash, name="update_cash"),
 
 ]
